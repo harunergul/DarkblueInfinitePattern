5b70b3c4aa93523640d9d33d74034e6eb61d559d
Harun ERGUL <harunergul@outlook.com>
2021-01-01 22:52:17 +0000

    newton method

--- main.scm
+++
@@ -1,3 +0,0 @@
-(define (average x y) (/ (+ x y) 2 ) )
-(display "harun")
-(average 47 68)
---
+++ newtonMethod.scm
@@ -0,0 +1,33 @@
+(display "Hello World")
+(newline)
+
+(define (average x y) (/ (+ x y) 2 ) )
+(display( average 5 7))
+(newline)
+
+(define (improve guess x) (average guess (/ x guess)) )
+(define (square a) (* a a) )
+(define (good_enough guess x)
+     
+   ( < (abs(- ( square guess ) x)) 0.0001)
+
+)
+
+(display (< (abs -25) 28) )
+
+
+;;(display  (good_enough 9 85.15) )
+
+
+(define (sqrt-iter guess x)
+    
+    (if (good_enough  guess x) 
+        guess 
+        (sqrt-iter (improve guess x) x)
+        
+    )    
+    
+
+)
+
+(display  (sqrt-iter 9 100) )
